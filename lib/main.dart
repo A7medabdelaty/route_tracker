c9ef088bdf065d4652a4a1c7517e97d1e92ea9e9
A7medabdelaty <elqahwagy.123@gmail.com>
2024-12-26 16:28:07 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:route_tracker/widgets/google_map_view.dart';
+
+void main() {
+  runApp(const RouteTracker());
+}
+
+class RouteTracker extends StatelessWidget {
+  const RouteTracker({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: GoogleMapView(),
+    );
+  }
+}
